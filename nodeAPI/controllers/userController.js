a9b0e1d91d4e798821c5d1e5d8698ac69384ef10
KTran-Tech <kevinmt191919@gmail.com>
2020-06-18 23:00:36 -0500

    User can get back their infoto see what they could update on their account

--- nodeAPI/controllers/userController.js
+++ nodeAPI/controllers/userController.js
@@ -1,15 +1,22 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utilities/catchAsync');
 
-exports.getUserById = catchAsync(
+exports.getMe = (req, res, next) => {
+  //If you are logged then req.user.id should already be available
+  //set the params id up so you dont have to manually add it
+  req.params.id = req.user.id;
+  next();
+};
+
+exports.getUser = catchAsync(
   async (req, res, next) => {
     const doc = await User.findById(req.params.id);
-
     if (!doc) {
       res.status(404).json({
         message: 'No document found with that ID',
       });
     }
+
     //
     /*send back the response as a json object(specially for 'status')
     because 'docs' is already one */
--- nodeAPI/routes/userRoute.js
+++ nodeAPI/routes/userRoute.js
@@ -9,9 +9,16 @@ const router = express.Router();
 router.use(authController.protect);
 //ANYTHING that comes AFTER this point will have this middleware applied
 
+//get all user along with their info
 router.get('/users', userController.getUsers);
 
-//get user based on ID
-router.get('/users/:id', userController.getUserById);
+//set params ID to current logged in user's Id so they don't have to do it manually
+//get user along with their info based on params ID and send it back
+//user use this info to see what they would like to update
+router.get(
+  '/users/myAccount',
+  userController.getMe,
+  userController.getUser
+);
 
 module.exports = router;
