c5de989b7c5f365ef72a1d4ec4f70c7f79df9ba6
KTran-Tech <kevinmt191919@gmail.com>
2020-06-21 21:22:21 -0500

    Really REALLY silly mistake now fixed

--- nodeAPI/controllers/postController.js
+++ nodeAPI/controllers/postController.js
@@ -17,7 +17,7 @@ exports.getPosts = catchAsync(
   }
 );
 
-exports.createPost = (req, res) => {
+exports.createPost = (req, res, next) => {
   /*When you are using form data like this one you have to 
   go in Postman and use 'x-www-form-urlencoded' instead 'raw'*/
   //formidable package method that will give us form fields
@@ -34,32 +34,9 @@ exports.createPost = (req, res) => {
   //we are not using catchAsync, therefore the 'err' parameter is there
   //parsing so that the 'form' method is able to read it
   form.parse(req, (err, fields, files) => {
-    //the fields is the form data sent in from user
-    const { title, body } = fields;
-
     if (err) {
       return res.status(400).json({
         error: 'Image could not upload',
-      });
-    }
-    if (
-      !title.value ||
-      (!title.value.length > 4 &&
-        !title.value.length < 150)
-    ) {
-      return res.status(400).json({
-        error:
-          'Please make sure there is a title and that it is between 4 to 150 characters',
-      });
-    }
-
-    if (
-      !body ||
-      (!body.length > 4 && !body.length < 2000)
-    ) {
-      return res.status(400).json({
-        error:
-          'Please make sure there is a body and that it is between 4 to 2000 characters',
       });
     }
 
@@ -69,6 +46,7 @@ exports.createPost = (req, res) => {
     //gives the post's ownership to user who has userID
     post.postedBy = req.user;
 
+    //
     //if photo exist
     if (files.photo) {
       //this is will read the photo's file path and assign it
@@ -78,6 +56,7 @@ exports.createPost = (req, res) => {
       //assign it to the photo type like jpg or png
       post.photo.contentType = files.photos.type;
     }
+    //
 
     // save newly created post to database
     post.save((errs, result) => {
@@ -91,5 +70,6 @@ exports.createPost = (req, res) => {
         message: 'success',
       });
     });
+    //
   });
 };
