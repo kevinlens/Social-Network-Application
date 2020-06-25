5a1cce632578b44e78f55fcbb96d688d548d9179
KTran-Tech <kevinmt191919@gmail.com>
2020-06-24 21:45:19 -0500

    React Front-End Setup

---
+++ react-front/src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    
+    render(){
+        return(
+            <section>
+                <h1>Hello World</h1>
+            </section>
+        )
+    }
+
+}
+
+export default App
