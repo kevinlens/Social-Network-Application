663fb1a21b881ca1747c7cf41c1ffad85861d131
KTran-Tech <kevinmt191919@gmail.com>
2020-06-25 17:37:13 -0500

    Adding the bootstrap layout for signup page

--- react-front/src/components/User/Signup.js
+++ react-front/src/components/User/Signup.js
@@ -1,13 +1,40 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 class Signup extends Component {
-    render(){
-        return (
-            <div>
-                <h2>Signup</h2>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      error: "",
+    };
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Signup</h2>
+
+        <form>
+          <div className="form-group">
+            <label className="text-muted">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Email</label>
+            <input type="email" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label className="text-muted">Password</label>
+            <input type="password" className="form-control" />
+          </div>
+          <button className="btn btn-raised btn-primary">
+              Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Signup;
