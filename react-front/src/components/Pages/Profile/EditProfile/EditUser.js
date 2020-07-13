855d9744ac5307ab6724fe3c72c72c29afcf2920
KTran-Tech <kevinmt191919@gmail.com>
2020-07-13 00:31:02 -0500

    Edit User Setup

---
+++ react-front/src/components/Pages/Profile/EditProfile/EditUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditUser extends Compoent {
+  render() {
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Edit Profile</h2>
+      </div>
+    );
+  }
+}
+
+export default EditProfile;
